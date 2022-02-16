bb26a909918ed68b7d44174c381dcfbef4cad54b
Karanpreet2001 <karanpreetfarwaha@gmail.com>
2022-02-16 12:35:47 -0800

    First Commit

--- app/src/main/java/com/example/servpro/MainActivity.java
+++ app/src/main/java/com/example/servpro/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Final Project
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
